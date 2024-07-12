7c5b636f1ad8317e550234fb59f0ab96dc2643eb
tdsrmd <demirsedatdemir@icloud.com>
2024-07-12 22:54:57 +0300

    feat: Add dotenv configuration for cookie
    This commit adds the necessary dotenv configuration to read the cookie value from the .env file. This allows the cookie value to be securely stored and accessed through the process environment variable. The cookie value is used in the login process for the LinkedIn service.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const Linkout = require("./lib/linkedin.service");
-const { url } = require("inspector");
+require("dotenv").config();
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -18,8 +18,7 @@ const { url } = require("inspector");
   await Linkout.tools.loadCursor(page, true);
 
   await Linkout.services.login(page, cdp, {
-    cookie:
-      "AQEDAUKJ1v0FqB3nAAABkKKdnnYAAAGQxqoidk0AwNaxW1v2ncvw0AzuLGoNOjRGbTgDUYNOXsGozlT1fR7VBOLQu7lQDnYuChgX-gf7czAf-AY-L9piXNIWf7xcpFv-BFKY0NnEz6LPvihEM2lKb-Lj",
+    cookie: process.env.COOKIE,
   });
 
   // await Linkout.services.getUsersUrl(page, cdp, {
@@ -27,13 +26,13 @@ const { url } = require("inspector");
   //   pageSize: 20,
   // });
 
-  const urls = fs.readFileSync("1.txt", "utf-8").split("\n");
-  for (const url of urls) {
-    if (url.trim()) {
-      await Linkout.services.like(page, cdp, {
-        url: url.trim(),
-      });
-    }
-  }
+  // const urls = fs.readFileSync("1.txt", "utf-8").split("\n");
+  // for (const url of urls) {
+  //   if (url.trim()) {
+  //     await Linkout.services.like(page, cdp, {
+  //       url: url.trim(),
+  //     });
+  //   }
+  // }
   browser.close();
 })();
