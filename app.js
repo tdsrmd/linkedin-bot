93a3279aec2a546022727fae47dc550a8c62e8df
tdsrmd <demirsedatdemir@icloud.com>
2024-07-12 22:47:47 +0300

    new service: getUsers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const Linkout = require("./lib/linkedin.service");
+const { url } = require("inspector");
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -18,16 +19,21 @@ const Linkout = require("./lib/linkedin.service");
 
   await Linkout.services.login(page, cdp, {
     cookie:
-      "AQEDAUKJ1v0BZN9jAAABkIgmvWIAAAGQrDNBYk0AQQnVSFOStRkPqVXJjPNoGtt5t9RRaYViSt6_iyd1rSXSFWqFuMn74JbTINL9tp_Y7xCHi1YAU8xMfBLuztbv5nvncVUuJayyLdrARggHfpvgSYpu",
+      "AQEDAUKJ1v0FqB3nAAABkKKdnnYAAAGQxqoidk0AwNaxW1v2ncvw0AzuLGoNOjRGbTgDUYNOXsGozlT1fR7VBOLQu7lQDnYuChgX-gf7czAf-AY-L9piXNIWf7xcpFv-BFKY0NnEz6LPvihEM2lKb-Lj",
   });
 
-  const urls = fs.readFileSync("1.txt", "utf-8").split("\n");
+  // await Linkout.services.getUsersUrl(page, cdp, {
+  //   url: "https://www.linkedin.com/search/results/people/?currentCompany=%5B%2211529916%22%5D&origin=FACETED_SEARCH&sid=sUK",
+  //   pageSize: 20,
+  // });
 
+  const urls = fs.readFileSync("1.txt", "utf-8").split("\n");
   for (const url of urls) {
     if (url.trim()) {
-      await Linkout.services.follow(page, cdp, {
+      await Linkout.services.like(page, cdp, {
         url: url.trim(),
       });
     }
   }
+  browser.close();
 })();
